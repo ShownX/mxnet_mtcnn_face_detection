7b61d5ec330766504b7e2f2664a2b9196dc4dec5
Xiang Xu <dellsford@gmail.com>
2019-02-25 14:33:34 -0600

    Update UHDB31 process

--- uhdb31_detect_align.py
+++ uhdb31_detect_align.py
@@ -44,9 +44,9 @@ def crop_align_images(items):
     for item in items:
         ext = os.path.splitext(item)[1]
         out_im = os.path.join(args.out_dir, args.sub_dir, item)
-        out_lm = os.path.join(args.out_dir, 'mtcnn', item.replace(ext, '.txt'))
-
-        if not os.path.exists(out_im):
+        out_box = item.replace(args.dir, os.path.join(args.out_dir, 'mtcnn')).replace(ext, '.box')
+        out_lm = item.replace(args.dir, os.path.join(args.out_dir, 'mtcnn')).replace(ext, '.lm')
+        if args.no_crop:
             try:
                 im = cv2.imread(os.path.join(args.dir, item))
 
@@ -57,62 +57,93 @@ def crop_align_images(items):
                     plt.imshow(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
                     plt.show()
 
-                if os.path.exists(out_lm):
-                    lm = np.loadtxt(out_lm)
-                else:
-                    results = detector.detect_face(im)
+                results = detector.detect_face(im)
 
-                    if results is not None:
-                        x, y = results[1][0][:5].reshape((5, 1)), results[1][0][5:].reshape((5, 1))
-                        lm = np.concatenate((x, y), axis=1)
-                    else:
-                        lm = None
+                if results is not None:
+                    box = results[0][0]
+                    x, y = results[1][0][:5].reshape((5, 1)), results[1][0][5:].reshape((5, 1))
+                    lm = np.concatenate((x, y), axis=1)
 
-                if lm is not None:
-                    if args.align:
-
-                        if args.show:
-                            plt.imshow(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
-                            plt.scatter(lm[:, 0], lm[:, 1])
-                            plt.show()
-
-                        trans.estimate(lm, dst)
-                        M = trans.params[0:2, :]
-
-                        crop_im = cv2.warpAffine(im, M, (args.size, args.size), borderValue=0)
-
-                    else:
-                        lm_mean = np.mean(lm, axis=0)
-                        lm_min = np.min(lm, axis=0)
-                        lm_max = np.max(lm, axis=0)
-                        edge = np.mean(lm_max - lm_min) * (1 + args.enlarge_factor)
-                        x1, y1, x2, y2 = int(lm_mean[0] - edge), int(lm_mean[1] - edge), \
-                                         int(lm_mean[0] + edge), int(lm_mean[1] + edge)
-
-                        x1 = max(0, min(x1, im.shape[1]))
-                        x2 = max(0, min(x2, im.shape[1]))
-                        y1 = min(im.shape[0], max(y1, 0))
-                        y2 = min(im.shape[0], max(y2, 0))
-                        crop_im = im[y1:y2, x1:x2]
-
-                    if not os.path.exists(os.path.dirname(out_im)):
-                        os.makedirs(os.path.dirname(out_im))
+                    if not os.path.exists(os.path.dirname(out_box)):
+                        os.makedirs(os.path.dirname(out_box))
 
                     if not os.path.exists(os.path.dirname(out_lm)):
                         os.makedirs(os.path.dirname(out_lm))
 
-                    if args.show:
-                        plt.imshow(cv2.cvtColor(crop_im, cv2.COLOR_BGR2RGB))
-                        plt.show()
-                        plt.pause(5)
-
-                    cv2.imwrite(out_im, crop_im)
-                    if not os.path.exists(out_lm):
-                        np.savetxt(out_lm, lm)
+                    np.savetxt(out_box, box)
+                    np.savetxt(out_lm, lm)
 
             except Exception as e:
                 # logging.warning(e)
                 continue
+        else:
+            if not os.path.exists(out_im):
+                try:
+                    im = cv2.imread(os.path.join(args.dir, item))
+
+                    if im is None:
+                        continue
+
+                    if args.show:
+                        plt.imshow(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
+                        plt.show()
+
+                    if os.path.exists(out_lm):
+                        lm = np.loadtxt(out_lm)
+                    else:
+                        results = detector.detect_face(im)
+
+                        if results is not None:
+                            x, y = results[1][0][:5].reshape((5, 1)), results[1][0][5:].reshape((5, 1))
+                            lm = np.concatenate((x, y), axis=1)
+                        else:
+                            lm = None
+
+                    if lm is not None:
+                        if args.align:
+
+                            if args.show:
+                                plt.imshow(cv2.cvtColor(im, cv2.COLOR_BGR2RGB))
+                                plt.scatter(lm[:, 0], lm[:, 1])
+                                plt.show()
+
+                            trans.estimate(lm, dst)
+                            M = trans.params[0:2, :]
+
+                            crop_im = cv2.warpAffine(im, M, (args.size, args.size), borderValue=0)
+
+                        else:
+                            lm_mean = np.mean(lm, axis=0)
+                            lm_min = np.min(lm, axis=0)
+                            lm_max = np.max(lm, axis=0)
+                            edge = np.mean(lm_max - lm_min) * (1 + args.enlarge_factor)
+                            x1, y1, x2, y2 = int(lm_mean[0] - edge), int(lm_mean[1] - edge), \
+                                             int(lm_mean[0] + edge), int(lm_mean[1] + edge)
+
+                            x1 = max(0, min(x1, im.shape[1]))
+                            x2 = max(0, min(x2, im.shape[1]))
+                            y1 = min(im.shape[0], max(y1, 0))
+                            y2 = min(im.shape[0], max(y2, 0))
+                            crop_im = im[y1:y2, x1:x2]
+
+                        if not os.path.exists(os.path.dirname(out_im)):
+                            os.makedirs(os.path.dirname(out_im))
+
+                        if not os.path.exists(os.path.dirname(out_lm)):
+                            os.makedirs(os.path.dirname(out_lm))
+
+                        if args.show:
+                            plt.imshow(cv2.cvtColor(crop_im, cv2.COLOR_BGR2RGB))
+                            plt.show()
+                            plt.pause(5)
+
+                        cv2.imwrite(out_im, crop_im)
+                        if not os.path.exists(out_lm):
+                            np.savetxt(out_lm, lm)
+
+                except Exception as e:
+                    # logging.warning(e)
+                    continue
 
         p_bar.update()
 
